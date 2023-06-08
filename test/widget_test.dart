5d4afa4d5c2fea6b908fc9cc89a543e9bb2554f7
raditya2010631170111 <2010631170111@student.unsika.ac.id>
2023-06-08 15:00:04 +0700

    1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tugaspraktikum8/tugaspraktikum8.dart';
+import 'package:uts/3_onlineshop_list.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
